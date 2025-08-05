f5d4a0bd3e107c3a0ce83046a1a12f2948510ac0
lapzenstore <lapzen.store@gmail.com>
2025-08-05 10:07:13 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+}
+
+module.exports = nextConfig
